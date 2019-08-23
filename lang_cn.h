03171f7ce6026750b67c1e146adc5aaf523343e0
CYD-o <changydong@gmail.com>
2019-08-24 02:36:52 +0800

    Update lang_cn.h

--- lang_cn.h
+++ lang_cn.h
@@ -24,18 +24,18 @@ const String TXT_PRECIPITATION_SOON = "Prec.";
 
 
 //Sun
-const String TXT_SUNRISE  = "Sunrise";
-const String TXT_SUNSET   = "Sunset";
+const String TXT_SUNRISE  = "日出";
+const String TXT_SUNSET   = "日落";
 
 //Moon
 const String TXT_MOON_NEW             = "New";
-const String TXT_MOON_WAXING_CRESCENT = "Waxing Crescent";
-const String TXT_MOON_FIRST_QUARTER   = "First Quarter";
-const String TXT_MOON_WAXING_GIBBOUS  = "Waxing Gibbous";
-const String TXT_MOON_FULL            = "Full";
-const String TXT_MOON_WANING_GIBBOUS  = "Waning Gibbous";
-const String TXT_MOON_THIRD_QUARTER   = "Third Quarter";
-const String TXT_MOON_WANING_CRESCENT = "Waning Crescent";
+const String TXT_MOON_WAXING_CRESCENT = "新月";
+const String TXT_MOON_FIRST_QUARTER   = "上弦月";
+const String TXT_MOON_WAXING_GIBBOUS  = "盈凸月";
+const String TXT_MOON_FULL            = "满月";
+const String TXT_MOON_WANING_GIBBOUS  = "亏凸月";
+const String TXT_MOON_THIRD_QUARTER   = "下弦月";
+const String TXT_MOON_WANING_CRESCENT = "残月";
 
 //Power / WiFi
 const String TXT_POWER  = "Power";
@@ -45,8 +45,8 @@ const char* TXT_UPDATED = "Updated:";
 
 //Wind
 const String TXT_WIND_SPEED_DIRECTION = "Wind Speed/Direction";
-const String TXT_N   = "N";
-const String TXT_NNE = "NNE";
+const String TXT_N   = "N";//北
+const String TXT_NNE = "NNE";//东北偏北
 const String TXT_NE  = "NE";
 const String TXT_ENE = "ENE";
 const String TXT_E   = "E";
